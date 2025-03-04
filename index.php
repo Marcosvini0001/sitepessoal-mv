98e1907c8c83e164e16ed01bd4cb490e55b2a25b
Marcos Vinicius <mvsenko7@gmail.com>
2025-03-04 20:56:35 -0300

    resp titulo principal - nome

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     <header>
       <div>
         <h2 id="h2-principal"><strong>Marcos Vinicius Bartoli Senko</strong></h2>
-        <h2><strong>Marcos Vinicius Bartoli Senko</strong></h2>
+        <h2 id="h2-resp"><strong>Marcos Vinicius Bartoli Senko</strong></h2>
         <p class="p-principal">
           <!--< 🔌 Conectando... Usuário detectado. />-->
           < Seja Bem-vindo ao meu portfólio! />
